5c557018224c70d05bf7504e8723ca97f27cb64a
reinholz36 <dreinholz28@gmail.com>
2021-12-19 11:34:03 -0500

    added hourly blocks to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 var currentDayEl = document.getElementById("currentDay")
-console.log("this worked")
 
+
+//pulls the current time by set format
 time = moment().format("dddd, MMMM Do");
 console.log("time raw data", time);
 console.log("time", time._d);
-
+//displays the day, month and date on header
 var currentDayDisplay = document.createElement ("p")
 currentDayDisplay.textContent = time;
 currentDayDisplay.className = "lead"
 currentDayEl.appendChild(currentDayDisplay);
+
+currentyHourly = moment().format("LT")
+console.log("hourly", currentyHourly);
+
+
