118442744aae13f1ddf5282d2a4c047a4675cde7
reinholz36 <dreinholz28@gmail.com>
2021-12-19 14:37:03 -0500

    cleaned js and added background img in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ var saveBtn = document.getElementsByClassName("saveBtn")
 
 //pulls the current time by set format
 time = moment().format("dddd, MMMM Do");
-console.log("time raw data", time);
-console.log("time", time._d);
+
 //displays the day, month and date on header
 var currentDayDisplay = document.createElement ("p")
 currentDayDisplay.textContent = time;
@@ -12,7 +11,7 @@ currentDayDisplay.className = "lead"
 currentDayEl.appendChild(currentDayDisplay);
 
 /*Save to local storage *Note: using "this" allows for dynamic use
-of button accrose all time block save buttons where as entering "saveBtn" for sibling parent only yeilds first button use*/
+of button accrose all save buttons where as entering "saveBtn" for sibling parent only yeilds first button use*/
 $(saveBtn).on("click", function(){
     var textEntry = $(this).siblings(".description").val();
     var timeBlock = $(this).parent().attr("id");
@@ -36,23 +35,20 @@ $("#17 .description").val(localStorage.getItem("17"));
 //Gets current local time
 currentyHourly = moment().hours();
 
+//Switch css color based on time
 $(".time-block").each(function() {
     var timeBlockHour = parseInt($(this).attr("id"));
 
     if (timeBlockHour < currentyHourly) {
+        $(this).removeClass("present future");
         $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
     }
-
     else if (timeBlockHour === currentyHourly) {
-        $(this).removeClass("past");
+        $(this).removeClass("past future");
         $(this).addClass("present");
-        $(this).removeClass("future");
 
     } else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
+        $(this).removeClass("past present");
         $(this).addClass("future");
     }
 })
